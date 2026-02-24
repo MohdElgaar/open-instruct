e84b3ec1bf63c834484409dc26d0ae9b8a024253
Mohamed Elgaar <mohdelgaar@gmail.com>
2026-02-24 13:20:29 -0500

    Remove existing 'index' column before adding new in combined_dataset

--- open_instruct/dataset_transformation.py
+++ open_instruct/dataset_transformation.py
@@ -1831,6 +1831,8 @@ class DatasetTransformationCache:
 
         # Combine datasets
         combined_dataset = concatenate_datasets(transformed_datasets)
+        if "index" in combined_dataset.column_names:
+            combined_dataset = combined_dataset.remove_columns("index")
         combined_dataset = combined_dataset.add_column("index", range(len(combined_dataset)))
         if dataset_skip_cache:
             return combined_dataset
@@ -1978,6 +1980,8 @@ class LocalDatasetTransformationCache:
 
         # Combine datasets
         combined_dataset = concatenate_datasets(transformed_datasets)
+        if "index" in combined_dataset.column_names:
+            combined_dataset = combined_dataset.remove_columns("index")
         combined_dataset = combined_dataset.add_column("index", range(len(combined_dataset)))
 
         # Prepare return statistics
